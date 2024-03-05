8501192d69647ccacd6c463ab31e44dd3639c3ed
raynara <raynara.gondin@fatec.sp.gov.br>
2024-03-05 11:24:38 -0300

    alteração Raynara

--- 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
+++ 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 	cout << "Estrutura de Dados - Fatec" << endl;
-	cout << " Alterado por Claudio Nunes \n";
+	cout << " Alterado por Raynara Gondin \n";
 
 	cout << "Tipos Numericos\n";
 	cout << "===============\n";
